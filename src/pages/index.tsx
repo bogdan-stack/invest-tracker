599d4934c71cf811d50f3ac2552af77b3b255abe
Bogdan <b.helerea@yahoo.com>
2023-04-02 07:14:28 +0300

    6th commit 1st day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,25 +22,25 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex justify-center h-screen">
         <div className=" h-full w-full bg-slate-100 border-x">
-        <div className=" flex justify-between w-full gap-4 bg-white border-x p-3 shadow-sm">
+        <div className=" flex justify-between w-full gap-4 bg-white border-x p-3 shadow-sm align-middle">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
         <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
           {!user.isSignedIn && <SignInButton><button className=" bg-red-600 text-white font-medium rounded-md p-2">Log In</button></SignInButton>}
-          {!!user.isSignedIn && <Heading textColor='black' fontWeight='bold'>Hello Bogdan!</Heading>}
+          {!!user.isSignedIn && <Text padding={3} textColor='black' fontWeight='bold'>Hello Bogdan!</Text>}
           {!!user.isSignedIn && <SignOutButton><button className=" bg-red-600 text-white font-medium rounded-md p-2">Log Out</button></SignOutButton>}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
         <Stack justifyContent='center' p={5}>
         <Card w='90%' alignSelf='center' backgroundColor='white'>
           <CardHeader>
-          <Heading fontSize='2xl'>Investments</Heading>
+          <Heading fontSize='md'>Investments</Heading>
           </CardHeader>
           <CardBody>
-          <Stack divider={<StackDivider />} spacing='4'>
+          <Stack divider={<StackDivider />} spacing='3'>
           {data?.map((post) => (
-          <HStack justifyContent='space-between'key={post.id}>
-            <Text textColor='black' fontWeight='medium' > {post.investAmount}</Text>
+          <HStack justifyContent='space-between' textAlign='center' key={post.id}>
+            <Text textColor='black' fontWeight='medium' > {post.investAmount} RON</Text>
             <Text textColor='gray.500' > {post.fondName} </Text>
             <Text textColor='gray.500'> {post.createdAt.toDateString()} </Text>
           </HStack>))}
