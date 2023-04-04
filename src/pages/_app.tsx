63a90b1ddbf01141ea3562ed7d1cb16fca2d5c4c
Bogdan <b.helerea@yahoo.com>
2023-04-04 20:49:48 +0300

    Changed font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import { type AppType } from "next/app";
-
 import { ClerkProvider } from "@clerk/nextjs";
 import { ChakraProvider } from "@chakra-ui/react";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import { Source_Sans_Pro } from '@next/font/google';
+
+
+const sourceSans = Source_Sans_Pro({
+  subsets: ['latin'],
+  weight: ['200','300','400','600','700'],
+});
+
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return  (
+    <main className={sourceSans.className}>
     <ChakraProvider>
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
     </ChakraProvider>
+    </main>
   );
 };
 
