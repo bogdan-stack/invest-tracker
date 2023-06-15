c347582fa2aa18de244ff3496f75df6ffb2ad261
Bogdan <b.helerea@yahoo.com>
2023-06-15 17:15:15 +0300

    implementing budget dashboard

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,8 +32,8 @@ const Footer = () => {
       });
 
       const getInfo = async () => {
-        //const res = await fetch('https://invest-tracker-nine.vercel.app/api/trpc/getInfo')
-        const res = await fetch("http://localhost:3000/api/trpc/getInfo");
+        const res = await fetch('https://invest-tracker-nine.vercel.app/api/trpc/getInfo')
+        //const res = await fetch("http://localhost:3000/api/trpc/getInfo");
         const {
           infoUfVal,
           infoFoName,
